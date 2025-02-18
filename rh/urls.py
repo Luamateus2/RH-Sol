6fed1d6bfb3c7f00fd5df0db07b1b6974638d2a4
Lua Mateus <104106208+Luamateus2@users.noreply.github.com>
2025-02-18 14:04:20 -0300

    Configuração das rotas

--- rh/urls.py
+++ rh/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import home_views
 
 urlpatterns = [
+    path('login',home_views.login,name='login page'),
+    path('signup',home_views.signup,name='page signup'),
     path('',home_views.home,name='home'),
 ]
--- rh/views/home_views.py
+++ rh/views/home_views.py
@@ -1,13 +1,14 @@
 from django.shortcuts  import render,redirect
-from ..models import Funcionario, Setor
+from ..models import Usuario,Funcionario, Setor
 
 def home(request):
     return render(request,'rh/index.html')
 
+def login(request):
+     return render(request,'rh/login.html')
 
-
-
-
+def  signup(request):
+     return render(request,'rh/signup.html')
 
 def cadastar_funcionario(request):
     if request.method =='POST':
