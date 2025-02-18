305d9d7d85e083a849371250acdc8df32dc963c4
Lua Mateus <104106208+Luamateus2@users.noreply.github.com>
2025-02-18 15:45:10 -0300

    Migração para Postgresql

--- setup/settings.py
+++ setup/settings.py
@@ -68,11 +68,14 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),  # Caso não haja DB_HOST no .env, usa localhost
+        'PORT': os.getenv('DB_PORT', '5432'),  # Caso não haja DB_PORT no .env, usa 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -109,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS=[
+    BASE_DIR,"static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
